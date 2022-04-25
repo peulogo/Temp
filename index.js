9f81db76f6a01825909c397b65af7d93866ff1c3
peulogo <99072707+peulogo@users.noreply.github.com>
2022-04-25 16:52:35 +0300

    anim4

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ let header = document.querySelector('.header');
 let heroTitle = document.querySelector('.hero-title');
 let select = document.querySelector('.hero-select');
 let selectButton = document.querySelector('.hero-button');
+let animItems = document.querySelectorAll('.apis-item');
 
-window.onload = async() => { 
+window.onload = () => { 
    setTimeout( () => {
     getHeder();
    }, 500)
@@ -39,3 +40,29 @@ let getSelectButton = () => {
     selectButton.classList.add('active');
 }
 
+
+
+
+window.addEventListener('scroll', animOnScroll)
+
+function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 4;
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+        if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemPoint)){
+            animItem.classList.add('active');
+        }
+    }
+}
+
+function offset(el) {
+    const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+}
+
+animOnScroll();
